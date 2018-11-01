b5193c37c12b8ecd96e349b6b5e585e643cc7bb5
André Formento <andreformento.sc@gmail.com>
2018-11-01 00:24:10 -0300

    Add swagger

---
+++ rest-with-swagger/README.md
@@ -0,0 +1,34 @@
+# clojure-rest
+
+## Prerequisites
+
+You will need [Leiningen][] 2.0.0 or above installed.
+
+[leiningen]: https://github.com/technomancy/leiningen
+
+## Running local
+
+Start Mongo
+```shell
+docker-compose up -d mongo
+```
+
+To start a web server for the application, run:
+
+```shell
+lein ring server 8080
+```
+
+## Running all with docker
+
+```shell
+docker-compose up --build
+```
+
+Go to http://localhost:8080
+
+## References
+
+- https://blog.interlinked.org/programming/clojure_rest.html
+- https://github.com/metosin/compojure-api/blob/master/examples/simple/README.md
+- https://github.com/metosin/compojure-api/blob/master/examples/resources/src/example/handler.clj
---
+++ rest-with-swagger/src/my_app/repository.clj
@@ -0,0 +1,37 @@
+(ns my-app.repository
+  (:require [monger.core :as mg]
+            [monger.collection :as mc]
+            [monger.json])
+  (:import [org.bson.types ObjectId]))
+
+(def conn (mg/connect {:host "localhost"}))
+
+(def db (mg/get-db conn "test"))
+
+(def people-collection "people")
+
+(defn objectId->str [e]
+  (if-let [objectId (:_id e)]
+    (-> e (dissoc :_id) (assoc :id (str objectId)))
+    e))
+
+(def objectIds->str (partial map objectId->str))
+
+(defn get-all-people []
+  (-> (mc/find-maps db people-collection)
+      objectIds->str))
+
+(defn get-person [id]
+  (-> (mc/find-map-by-id db people-collection (ObjectId. id))
+      objectId->str))
+
+(defn delete-person [id]
+  (mc/remove-by-id db people-collection (ObjectId. id)))
+
+(defn insert-person [person]
+  (-> (mc/insert-and-return db people-collection (assoc person :_id (ObjectId.)))
+      objectId->str))
+
+(defn update-person [id person]
+  (-> (mc/save-and-return db people-collection (assoc person :_id (ObjectId. id)))
+      objectId->str))
