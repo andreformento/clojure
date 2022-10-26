3cd95fb554b92f5e2b3e0cae259d5321cf055105
André Formento <andreformento.sc@gmail.com>
2022-10-25 21:38:51 -0300

    https://www.braveclojure.com/core-functions-in-depth/#Infinite_Sequences_

--- clojure-noob/src/clojure_noob/02_core_functions_in_depth.clj
+++ clojure-noob/src/clojure_noob/02_core_functions_in_depth.clj
@@ -43,3 +43,72 @@
         {}
         {:max 30 :min 10})
 
+(reduce (fn [new-map [key val]]
+          (if (> val 4)
+            (assoc new-map key val)
+            new-map))
+        {}
+        {:human 4.1
+         :critter 3.9})
+
+(def food-journal
+  [{:month 1 :day 1 :human 5.3 :critter 2.3}
+   {:month 1 :day 2 :human 5.1 :critter 2.0}
+   {:month 2 :day 1 :human 4.9 :critter 2.1}
+   {:month 2 :day 2 :human 5.0 :critter 2.5}
+   {:month 3 :day 1 :human 4.2 :critter 3.3}
+   {:month 3 :day 2 :human 4.0 :critter 3.8}
+   {:month 4 :day 1 :human 3.7 :critter 3.9}
+   {:month 4 :day 2 :human 3.7 :critter 3.6}])
+
+(take-while #(< (:month %) 3) food-journal)
+; => ({:month 1, :day 1, :human 5.3, :critter 2.3}
+; =>  {:month 1, :day 2, :human 5.1, :critter 2.0}
+; =>  {:month 2, :day 1, :human 4.9, :critter 2.1}
+; =>  {:month 2, :day 2, :human 5.0, :critter 2.5})
+
+(drop-while #(< (:month %) 3) food-journal)
+; => ({:month 3, :day 1, :human 4.2, :critter 3.3}
+; =>  {:month 3, :day 2, :human 4.0, :critter 3.8}
+; =>  {:month 4, :day 1, :human 3.7, :critter 3.9}
+; =>  {:month 4, :day 2, :human 3.7, :critter 3.6})
+
+(filter #(< (:human %) 5) food-journal)
+(some #(> (:critter %) 3) food-journal)
+(some #(and (> (:critter %) 3) %) food-journal)
+; => {:month 3, :day 1, :human 4.2, :critter 3.3}
+
+(def reverse-count #(* -1 (count %)))
+(sort-by reverse-count ["aaa" "c" "bb"])
+; => ("aaa" "bb" "c")
+
+(def vampire-database
+  {0 {:makes-blood-puns? false, :has-pulse? true :name "McFishwich"}
+   1 {:makes-blood-puns? false, :has-pulse? true  :name "McMackson"}
+   2 {:makes-blood-puns? true,  :has-pulse? false :name "Damon Salvatore"}
+   3 {:makes-blood-puns? true,  :has-pulse? true  :name "Mickey Mouse"}})
+
+(defn vampire-related-details
+  [social-security-number]
+  (Thread/sleep 1000)
+  (get vampire-database social-security-number))
+
+(defn vampire?
+  [record]
+  (and (:makes-blood-puns? record)
+       (not (:has-pulse? record))
+       record))
+
+(defn identify-vampire
+  [social-security-numbers]
+  (first (filter vampire?
+                 (map vampire-related-details social-security-numbers))))
+(time (vampire-related-details 0))
+; => "Elapsed time: 1000.139755 msecs"
+; => {:makes-blood-puns? false, :has-pulse? true, :name "McFishwich"}
+(time (def mapped-details (map vampire-related-details (range 0 1000000))))
+; => "Elapsed time: 0.030517 msecs"
+; => #'clojure-noob.core/mapped-details
+(time (first mapped-details))
+; => "Elapsed time: 32007.859939 msecs"
+; => {:makes-blood-puns? false, :has-pulse? true, :name "McFishwich"}
