df7eb4a149e6111f2909d19310d9bbbe0a31e11d
André Formento <andreformento.sc@gmail.com>
2023-03-25 11:09:48 -0300

    #Macros

--- read-and-eval/src/read_and_eval/core.clj
+++ read-and-eval/src/read_and_eval/core.clj
@@ -5,3 +5,21 @@
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+(read-string "(1 + 1)")
+
+(let [infix (read-string "(1 + 1)")]
+  (list (second infix) (first infix) (last infix)))
+
+(eval (let [infix (read-string "(1 + 1)")]
+        (list (second infix) (first infix) (last infix))))
+
+(defmacro ignore-last-operand
+  [function-call]
+  (println (str "function-call <"
+                (type function-call)
+                "> is: "
+                function-call))
+  (butlast function-call))
+
+(ignore-last-operand (+ 1 2 10))
