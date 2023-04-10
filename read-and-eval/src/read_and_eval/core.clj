f1bbd74468fe444ed0c9019234f49f462d0b5128
André Formento <andreformento.sc@gmail.com>
2023-04-10 15:40:32 -0300

    wip

--- read-and-eval/src/read_and_eval/core.clj
+++ read-and-eval/src/read_and_eval/core.clj
@@ -23,3 +23,27 @@
   (butlast function-call))
 
 (ignore-last-operand (+ 1 2 10))
+
+(defmacro infix
+  "this is a list -> [1 + 2]"
+  [infixed]
+  (let [op (second infixed)
+        x (first infixed)
+        y (last infixed)
+        result (list op x y)]
+    (println (str "result = " result " -> op=" op "; x="x "; y="y))
+    result))
+(infix (1 + 2))
+; 3
+
+
+(defmacro resolve-expression
+  [[x1 f-plus x2 f-* x3 f-minus x4]]
+  ;(infix ((infix (x1 f-plus (infix (x2 f-* x3)))) f-minus x4))
+  (println "x2" x2)
+  (println "x3" x3)
+  (println "re" (f-* 3 4))
+  (infix (x2 f-* x3)))
+
+(println (eval (resolve-expression (1 + 3 * 4 - 5))))
+; 8
