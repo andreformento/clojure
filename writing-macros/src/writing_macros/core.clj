7a68f7e957643126aca6e08f2ecc3e0cdac29a8f
André Formento <andreformento.sc@gmail.com>
2023-05-15 21:55:59 -0300

    https://www.braveclojure.com/writing-macros/#Validation_Functions

--- writing-macros/src/writing_macros/core.clj
+++ writing-macros/src/writing_macros/core.clj
@@ -92,7 +92,7 @@
               "Sweet gorilla of Manila, this is good code:"
               (list 'quote good))))
 
-(defmacro code-critic-togheter
+(defmacro code-critic-together
   "Phrases are courtesy Hermes Conrad from Futurama"
   [bad good]
   `(do (println "Great squid of Madrid, this is bad code:"
@@ -104,11 +104,20 @@
   [criticism code]
   `(println ~criticism (quote ~code)))
 
-(defmacro code-critic
+(defmacro code-critic-refactor
   [bad good]
   `(do ~(criticize-code "Cursed bacteria of Liberia, this is bad code:" bad)
        ~(criticize-code "Sweet sacred boa of Western and Eastern Samoa, this is good code:" good)))
 
+(defmacro code-critic
+  [bad good]
+  `(do ~@(map #(apply criticize-code %)
+              [["Cursed bacteria of Liberia, this is bad code:" bad]
+               ["Sweet sacred boa of Western and Eastern Samoa, this is good code:" good]])))
+
 (code-critic (1 + 1) (+ 1 1))
 ; => Great squid of Madrid, this is bad code: (1 + 1)
 ; => Sweet gorilla of Manila, this is good code: (+ 1 1)
+
+; Validation functions
+
